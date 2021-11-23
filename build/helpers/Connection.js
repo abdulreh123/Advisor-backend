f1de5c7ac7463ef3537486e6b6deec01addcd336
abdulreh123 <60946339+abdulreh123@users.noreply.github.com>
2021-11-23 16:23:51 +0200

    added pm2

--- build/helpers/Connection.js
+++ build/helpers/Connection.js
@@ -1,6 +1,6 @@
 "use strict";
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('advisor', 'app', 'Hd9msAwqPyd', {
+const sequelize = new Sequelize('advisor', 'app', 'VohkxZleque', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
