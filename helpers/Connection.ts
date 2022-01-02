5ebd29034eb92b865bc81c8b441d9015e7cca96e
unknown <abdulrehman.siddiqui@gunsel.com.tr>
2022-01-02 20:31:46 +0200

    added frontend

--- helpers/Connection.ts
+++ helpers/Connection.ts
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
     
-const sequelize = new Sequelize('advisor', 'root', '', {
+const sequelize = new Sequelize('advisor', 'app', 'VohkxZleque', {
     host: 'localhost',
     dialect:'mysql',
     logging: false
