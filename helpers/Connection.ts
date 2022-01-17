6679128ea1e88c07515a1b29ffd5c6d6f15316be
abdulreh123 <60946339+abdulreh123@users.noreply.github.com>
2022-01-17 12:23:27 +0200

    Update Connection.ts

--- helpers/Connection.ts
+++ helpers/Connection.ts
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
 
-    
-const sequelize = new Sequelize('advisor', 'root', '', {
+    VohkxZlequeconst sequelize = new Sequelize('advisor', 'app', 'VohkxZleque, {
     host: 'localhost',
     dialect:'mysql',
     logging: false
