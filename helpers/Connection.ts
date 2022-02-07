cdd5172ae76b482c5ce3969581a7222eb38e5bc7
abdulreh123 <60946339+abdulreh123@users.noreply.github.com>
2022-02-07 15:56:29 +0200

    added cgpa prediction

--- helpers/Connection.ts
+++ helpers/Connection.ts
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-    const sequelize = new Sequelize('Advisor_new', 'root', 'VohkxZleque', {
+    const sequelize = new Sequelize('Advisor_New', 'app', 'VohkxZleque', {
     host: 'localhost',
     port: 3306,
     dialect:'mysql',
