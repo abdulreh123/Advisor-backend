49221b063c4dad050422b4e34919c7d22ce29c79
abdul <abdulrehman.siddiqui@gunsel.com.tr>
2022-09-18 01:34:52 +0300

    deploy

--- build/helpers/Connection.js
+++ build/helpers/Connection.js
@@ -1,7 +1,7 @@
 "use strict";
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('Advisor_New', 'app', 'VohkxZleque', {
-    host: 'localhost',
+const sequelize = new Sequelize('advisor_new', 'abdul', 'ar12d.a.r', {
+    host: 'advsor.mysql.database.azure.com',
     port: 3306,
     dialect: 'mysql',
     logging: false
--- helpers/Connection.ts
+++ helpers/Connection.ts
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-    const sequelize = new Sequelize('advisor', 'root', '', {
-    host: 'localhost',
+    const sequelize = new Sequelize('advisor_new', 'abdul', 'ar12d.a.r', {
+    host: 'advsor.mysql.database.azure.com',
     port: 3306,
     dialect:'mysql',
     logging: false
